be84e84ffd42d6aedf5bd3f47a0d5af3742e2268
Kevin <691208yz@gmail.com>
2020-06-11 16:45:35 -0400

    auth and validation

---
+++ functions/dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: new Date().toISOString(),
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
