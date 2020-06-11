af22fb51c20e0bf2cc3d8ca0c80dba946c5bcaf2
Kevin <691208yz@gmail.com>
2020-06-11 14:34:15 -0400

    app init and create/show messages functions

---
+++ functions/index.js
@@ -0,0 +1,57 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../socialapp-12ef9-firebase-adminsdk-pl9i4-056d1d9c8e.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://socialapp-12ef9.firebaseio.com",
+});
+
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req, res) => {
+    admin
+      .firestore()
+      .collection("screams")
+      .orderBy('createdAt', 'desc')
+      .get()
+      .then((data) => {
+        let screams = [];
+        data.forEach((doc) => {
+          screams.push({
+              screamId: doc.id,
+              body: doc.data().body,
+              userHandle: doc.data().userHandle,
+              createdAt: new Date().toISOString(),
+          });
+        });
+        return res.json(screams);
+      })
+      .catch((err) => console.error(err));
+    
+})
+
+
+app.post('/scream', (req, res) => {
+
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    };
+
+    admin.firestore()
+        .collection('screams')
+        .add(newScream)
+        .then((doc) => {
+            return res.json({ message: `document ${doc.id} created`});
+        })
+        .catch((err) => {
+            res.status(500).json({error: 'something went wrong'})
+            console.error(err);
+        })
+});
+
+exports.api = functions.https.onRequest(app);
